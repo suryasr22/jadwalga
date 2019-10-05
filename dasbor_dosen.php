973ac701a68f70dae311b56e73b41cc402b423be
suryasr22 <55946789+suryasr22@users.noreply.github.com>
2019-10-05 17:08:37 +0700

    Dashboard Dosen FE

--- dasbor_dosen.php
+++ dasbor_dosen.php
@@ -16,112 +16,106 @@
     }
   }
 
-
-
 ?>
 
+
 <!DOCTYPE html>
+<html>
+
 <head>
-    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
-  <link rel="stylesheet" type="text/css" href="style.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
   <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="Dashboard">
-    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-  	<!-- my script -->
-    <script type="text/javascript">
-    //JQUERY
-    $(document).ready(function(){
-      //search
-      $("#search").keyup(function(){
-      var search = document.getElementById("search").value;
+  <title>Dahsboard Dosen</title>
 
-      //console.log("ajax/uname_status.php?uname=" + uname);
-        $.get("ajax/search_dokter.php?q=" + search, function(data, status){
-          $("tbody").html(data);
-        });
-      });
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-      $('#btneditmakul').click(function() {
-        $.get($(this).attr('href'), function(msg){
-          $("#edit_makul-konten").html(msg);
-          $('#editmakul').modal('show');
-        });
-        return false;
-      });
-    });
-    </script>
+
 </head>
-<body class="vh-100 w-75 mx-auto shadow p-3 mb-5 bg-white rounded">
-    <div class="container  col-7 w-50">
-    		<ul class="nav nav-tabs ">
-    			<li class="nav-item active"><a data-toggle="tab" class="nav-link active" href="#makul">Mata Kuliah</a></li>
-    			<li class="nav-item"><a data-toggle="tab" class="nav-link" href="#editakun">Edit Akun</a></li>
-    		</ul>
 
-        <!--konten-->
-    		<div class="tab-content align-middle">
-          <div class="modal fade" id="editmakul" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="judul">Tambah Mata Kuliah</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
+<body class="w-75 mx-auto shadow-lg bg-white">
 
-                <div class="modal-body" id="edit_makul-konten">
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top shadow-sm">
+    <div class="container">
+      <a class="navbar-brand" href="#">Dashboard Dosen</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Jadwal</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Profile</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-              </div>
-            </div>
-          </div>
+  <!-- Page Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <hr>
+        <h1 class="mt-5 text-center">Jadwal</h1>
+              
+        <table class="table w-auto mx-auto table-dark table-striped table-bordered table-advance table-hover table-responsive-sm col-lg-12">
+          <thead>
+            <th>Nama Mata Kuliah</th>
+            <th>Semester</th>
+            <th>Sks</th>
+          </thead>
+          <tbody>
+            <?php
+            while($makul = $dataMakul->fetch_assoc()){
+              echo "
+                <tr>
+                  <td>
+                    $makul[nama]
+                  </td>
+                  <td>
+                    $makul[semester]
+                  </td>
+                  <td>
+                    $makul[sks]
+                  </td>
+              ";
 
-          <table class="table table-striped table-advance table-hover table-condensed">
-            <thead>
-              <th>Mata Kuliah</th>
-              <th>SKS</th>
-            </thead>
-            <tbody>
-                <?php
-                while($makul = $dataMakul->fetch_assoc()){
-                echo "
-                  <tr>
-                    <td>
-                      $makul[nama]
-                    </td>
-                    <td>
-                      $makul[sks]
-                    </td>
-                   ";
+              echo "
+                  <td align =\"right\">
+                    <a onclick =\"return confirm('Yakin Ingin menghapus data?')\" href=\"hapus_makul.php?id_dosen=$makul[id]\" class=\"btn btn-danger btn-sm\" role=\"button\"><i class=\"fa fa-trash-o\"></i></a>
+                  </td>
+                </tr>
+              ";
+            }
+            ?>
+          </tbody>
+        </table>
+        <button type="button" class="btn btn-primary">
+          Tambah
+        </button>
+      </div>
+    </div>
+  </div>
 
-                echo "
-                    <td align =\"right\">
-                      <a href=\"ajax/edit_makul.php?id=$makul[id]\" id=\"btneditmakul\" class=\"btn btn-primary btn-xs\" role=\"button\" data-toggle=\"#modal\" data-target=\"#editmakul\"><i class=\"fa fa-pencil\"></i></a>
-                      <a onclick =\"return confirm('Yakin Ingin menghapus data?')\" href=\"/process/hapus_makul.php?id_makul=$makul[id]\" class=\"btn btn-danger btn-xs\" role=\"button\"><i class=\"fa fa-trash-o\"></i></a>
-                    </td>
-                  </tr>
-                   ";
-                  }
-                ?>
-            </tbody>
-          </table>
-        </div>
-       
-        <!--Tabs-->
-    		<div id="editakun" class="tab-pane fade">
-          <?php include('tab_edit-akun.php'); ?>
-    		</div>   
-  	</div>
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.slim.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
 </body>
+
+</html>
--- process/edit_makul.php
+++ process/edit_makul.php
@@ -35,39 +35,67 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="Dashboard">
-    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+<html>
 
-    <title>Edit Makul</title>
+<head>
 
-    <!-- Bootstrap core CSS -->
-    <link href="../assets/css/bootstrap.css" rel="stylesheet">
-    <!--external css-->
-    <link href="../assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <!-- Custom styles for this template -->
-    <link href="../assets/css/style.css" rel="stylesheet">
-    <link href="../assets/css/style-responsive.css" rel="stylesheet">
+  <title>Dahsboard Dosen</title>
 
-    <!-- Offline JQuery -->
-    <script src="../assets/js/jquery-3.2.1.min.js"></script>
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-    <!-- Our Javascript -->
-    <script src="../assets/js/ours/validation_edit.js"></script>
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+</head>
 
-  <body>
+<body class="w-75 mx-auto shadow-lg bg-white">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top ">
+    <div class="container">
+      <a class="navbar-brand" href="#">Dashboard Dosen</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Mata Kuliah
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Dosen</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Jadwal</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Profile</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container mh-100">
+    <div class="row">
+      <div class="col-lg-12">
+        <h1 class="mt-5 text-center">Edit Mata Kuliah</h1>
+
   <section id="container" >
       <!--main content start-->
       <section id="main-content">
