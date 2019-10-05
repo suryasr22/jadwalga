97615eb287621423f0df286e0024e4a0cc31a994
suryasr22 <55946789+suryasr22@users.noreply.github.com>
2019-10-05 14:15:18 +0700

    gk nampak

--- dasbor_dosen.php
+++ dasbor_dosen.php
@@ -23,13 +23,16 @@
 <!DOCTYPE html>
 <head>
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
-  	<link rel="stylesheet" type="text/css" href="style.css">
-  	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <link href="css/font-awesome/css/font-awesome.css" rel="stylesheet" />
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
 
   	<!-- my script -->
     <script type="text/javascript">
---
+++ templete.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<head>
+	
+  	 <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina"> 
+
+</head>
+<body>
+	<!--Nav-->
+	<div class="container w-75">
+		<nav class="navbar navbar-dark bg-dark">
+  		<!-- Navbar content -->
+  		<div class="collapse navbar-collapse" id="navbarText">
+		    <span class="navbar-text">
+      		Dashboard Dosen
+    		</span>
+		    <ul class="navbar-nav mr-auto">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Mata Kuliah<span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Dosen</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Jadwal</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Profil</a>
+		      </li>
+		    </ul>
+  		</div>
+	</div>
+	
+
+
+	</nav>
+
+</body>
